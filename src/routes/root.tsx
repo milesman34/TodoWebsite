e3e4079cdb00fff1bb970c8da5d418420c1fdf92
mcsaunders02 <milesmanmusic@yahoo.com>
2024-09-17 13:10:59 -0400

    Initial version with css

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,3 +1,30 @@
 export const Root = () => {
-    return <div>Root</div>;
+    return (
+        <div
+            style={{
+                backgroundColor: "#1e2028",
+                width: "100%",
+                height: "100vh",
+                display: "flex",
+                flexDirection: "row"
+            }}
+        >
+            <div id="header" style={{
+                backgroundColor: "#26293a",
+                width: "100%",
+                height: "80px",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center"
+            }}>
+                <p style={{
+                    marginLeft: "10px",
+                    color: "white",
+                    fontFamily: "Inter",
+                    fontSize: "32px",
+                    fontWeight: "bold"
+                }}>Todo Website</p>
+            </div>
+        </div>
+    );
 };
