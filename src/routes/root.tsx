590bece625bcc36de490ebc13450e48a5f778ff0
mcsaunders02 <milesmanmusic@yahoo.com>
2024-09-17 21:37:12 -0400

    Make styling easier to work with

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,39 +1,37 @@
+import { CSSProperties } from "react";
 import { inter } from "../styles/classes";
 import { backgroundColor, headerColor } from "../styles/colors";
 
+const backgroundStyle: CSSProperties = {
+    backgroundColor: backgroundColor,
+    width: "100%",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "row"
+};
+
+const headerStyle: CSSProperties = {
+    backgroundColor: headerColor,
+    width: "100%",
+    height: "80px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center"
+};
+
+const headerTextStyle: CSSProperties = {
+    marginLeft: "10px",
+    color: "white",
+    fontSize: "32px",
+    fontWeight: "bold",
+    ...inter
+};
+
 export const Root = () => {
     return (
-        <div
-            id="background"
-            style={{
-                backgroundColor: backgroundColor,
-                width: "100%",
-                height: "100vh",
-                display: "flex",
-                flexDirection: "row"
-            }}
-        >
-            <div
-                id="header"
-                style={{
-                    backgroundColor: headerColor,
-                    width: "100%",
-                    height: "80px",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center"
-                }}
-            >
-                <div
-                    id="header-text"
-                    style={{
-                        marginLeft: "10px",
-                        color: "white",
-                        fontSize: "32px",
-                        fontWeight: "bold",
-                        ...inter
-                    }}
-                >
+        <div id="background" style={backgroundStyle}>
+            <div id="header" style={headerStyle}>
+                <div id="header-text" style={headerTextStyle}>
                     Todo Website
                 </div>
             </div>
