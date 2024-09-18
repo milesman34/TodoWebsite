0e2a2e917db64f551b157a780a61f98432b5b3ee
mcsaunders02 <milesmanmusic@yahoo.com>
2024-09-17 22:09:15 -0400

    Styles for buttons

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,39 +1,18 @@
-import { CSSProperties } from "react";
-import { inter } from "../styles/classes";
-import { backgroundColor, headerColor } from "../styles/colors";
-
-const backgroundStyle: CSSProperties = {
-    backgroundColor: backgroundColor,
-    width: "100%",
-    height: "100vh",
-    display: "flex",
-    flexDirection: "row"
-};
-
-const headerStyle: CSSProperties = {
-    backgroundColor: headerColor,
-    width: "100%",
-    height: "80px",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center"
-};
-
-const headerTextStyle: CSSProperties = {
-    marginLeft: "10px",
-    color: "white",
-    fontSize: "32px",
-    fontWeight: "bold",
-    ...inter
-};
+import "./root.css";
 
 export const Root = () => {
     return (
-        <div id="background" style={backgroundStyle}>
-            <div id="header" style={headerStyle}>
-                <div id="header-text" style={headerTextStyle}>
+        <div id="background">
+            <div id="header">
+                <div id="header-text">
                     Todo Website
                 </div>
+            </div>
+
+            <div id="left-sidebar">
+                <button id="all-tasks-button" className="tasks-button">All Tasks</button>
+
+                <button id="ungrouped-tasks-button" className="tasks-button">Ungrouped Tasks</button>
             </div>
         </div>
     );
