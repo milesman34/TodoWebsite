cb36bbd9991ef1b55af21024df69fdd0eb37d31e
mcsaunders02 <milesmanmusic@yahoo.com>
2024-09-19 18:13:36 -0400

    User issue #1 resolved

---
+++ src/features/taskGroups/sidebar/TaskGroupSidebar.test.tsx
@@ -0,0 +1,106 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { TaskGroupSidebar } from "./TaskGroupSidebar";
+
+import { afterEach, describe, expect, Mock, test, vi } from "vitest";
+import { nanoid } from "nanoid";
+import { Provider } from "react-redux";
+import { createStore } from "../../../app/store";
+
+vi.mock("nanoid", () => ({
+    nanoid: vi.fn()
+}));
+
+describe("TaskGroupSidebar", () => {
+    afterEach(() => {
+        cleanup();
+    });
+
+    describe("User must be able to press the Add button to create a new Task Group", () => {
+        test("Add Task Group button adds a new Task Group (with no task groups added yet)", async () => {
+            // Mock return values from nanoid + prompt
+            (nanoid as Mock).mockImplementation(() => "id1");
+
+            vi.stubGlobal("prompt", () => "First Task Group");
+
+            // Render the TaskGroupSidebar
+            render(
+                <Provider store={createStore()}>
+                    <TaskGroupSidebar />
+                </Provider>
+            );
+
+            await userEvent.click(screen.getByTestId("add-task-group-button"));
+
+            // Make sure it has the new task group
+            expect(screen.getByTestId("task-group-component-id1")?.textContent).toBe(
+                "First Task Group"
+            );
+        });
+
+        test("Add Task Group button adding multiple task groups", async () => {
+            // Render the TaskGroupSidebar
+            render(
+                <Provider store={createStore()}>
+                    <TaskGroupSidebar />
+                </Provider>
+            );
+
+            for (const pair of [
+                { id: "id1", name: "First Task Group" },
+                { id: "id2", name: "Second Task Group" }
+            ]) {
+                (nanoid as Mock).mockImplementation(() => pair.id);
+
+                vi.stubGlobal("prompt", () => pair.name);
+
+                await userEvent.click(screen.getByTestId("add-task-group-button"));
+            }
+
+            // Make sure it has the new task groups
+            expect(screen.getByTestId("task-group-component-id1")?.textContent).toBe(
+                "First Task Group"
+            );
+
+            expect(screen.getByTestId("task-group-component-id2")?.textContent).toBe(
+                "Second Task Group"
+            );
+        });
+
+        test("Add Task Group button does not add a new task if the prompt result is empty", async () => {
+            const store = createStore();
+
+            // Render the TaskGroupSidebar
+            render(
+                <Provider store={store}>
+                    <TaskGroupSidebar />
+                </Provider>
+            );
+
+            vi.stubGlobal("prompt", () => "");
+
+            await userEvent.click(screen.getByTestId("add-task-group-button"));
+
+            // There isn't a great way to test for the absence of an element, so instead check the store directly
+            expect(store.getState().groups).toEqual([]);
+        });
+
+        test("Add Task Group button does not add a new task if the prompt was exited", async () => {
+            const store = createStore();
+
+            // Render the TaskGroupSidebar
+            render(
+                <Provider store={store}>
+                    <TaskGroupSidebar />
+                </Provider>
+            );
+
+            vi.stubGlobal("prompt", () => null);
+
+            await userEvent.click(screen.getByTestId("add-task-group-button"));
+
+            // There isn't a great way to test for the absence of an element, so instead check the store directly
+            expect(store.getState().groups).toEqual([]);
+        });
+    });
+});
