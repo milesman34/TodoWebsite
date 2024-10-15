1ef30a84941bab84f5cab48e850be41e76817053
mcsaunders02 <milesmanmusic@yahoo.com>
2024-10-15 09:45:14 -0400

    Refactored header into own component

---
+++ src/features/header/AppHeader.tsx
@@ -0,0 +1,12 @@
+import "./AppHeader.css";
+
+/**
+ * This is the header for the app.
+ */
+export const AppHeader = () => {
+    return (
+        <div id="header">
+            <div id="header-text">Todo Website</div>
+        </div>
+    );
+};
